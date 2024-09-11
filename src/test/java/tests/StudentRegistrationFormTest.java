4875e500d20e361dd103d19be7fb38979bf5328a
Artem Elizarkov <elizarkov.av@gmail.com>
2024-09-11 16:46:17 +0300

    fix test for form

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -1,13 +1,11 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.JavascriptExecutor;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -26,29 +24,56 @@ public class StudentRegistrationFormTest {
         String name = "User";
         String lastName = "Testov";
         String email = "test@email.com";
-        String mobile = "+799999999";
+        String mobile = "7999999999";
         String subject = "Computer";
         String address = "Default city 2 Pushkin's Street";
+        String state = "Haryana";
+        String city = "Karnal";
+        String gender = "Female";
+        String hobby = "Reading";
+        String date = "29 September,1992";
+        String picName = "pic.jpg";
 
         //act
         open("/automation-practice-form"); //открыть страницу с формой
         $("#firstName").setValue(name); //ввести имя
         $("#lastName").setValue(lastName); //ввести фамилию
         $("#userEmail").setValue(email); //ввести электронную почту
-        $("#genterWrapper .custom-control:nth-child(2) label").click(); //выбрать женский пол
+        $(byText(gender)).click(); //выбрать женский пол
         $("#userNumber").setValue(mobile); //ввести номер телефона
 
         //выставление даты в календаре react-datepicker
-        JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
-        js.executeScript("$(\"#dateOfBirthInput\")[0].value='01/06/1992'");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").find("option[value='9']").click();
+        $(".react-datepicker__year-select").find("option[value='1992']").click();
+        $(".react-datepicker__week").find(".react-datepicker__day--029").click();
 
         $("#subjectsInput").setValue(subject).pressEnter(); //выбор в списке значения Computer Science
-        $("[for='hobbies-checkbox-2']").click(); //выбор хобби Reading
+        $(byText(hobby)).click(); //выбор хобби Reading
+        $("#uploadPicture").uploadFromClasspath("images/pic.jpg"); //загрузка изображения
+        $("#currentAddress").setValue(address); //ввод адреса
 
-        $("##uploadPicture").uploadFromClasspath("src/test/resources/pic.png");
+        //выбор state и city
+        $("#state").scrollTo(); //скролл страницы до инпута
 
-        System.out.println("test");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click(); //выбор state
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click(); //выбор city
 
+        $("#submit").click(); //нажать submit
+
+        //asserts
+        $(".table-responsive").shouldHave(text(name + " " + lastName));
+        $(".table-responsive").shouldHave(text(email));
+        $(".table-responsive").shouldHave(text(gender));
+        $(".table-responsive").shouldHave(text(mobile));
+        $(".table-responsive").shouldHave(text(date));
+        $(".table-responsive").shouldHave(text(subject));
+        $(".table-responsive").shouldHave(text(hobby));
+        $(".table-responsive").shouldHave(text(picName));
+        $(".table-responsive").shouldHave(text(address));
+        $(".table-responsive").shouldHave(text(state + " " + city));
 
     }
 }
